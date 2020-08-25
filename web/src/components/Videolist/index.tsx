e2a13be25505338fe34d20b95b4de6fab2530c7e
Casper <joeharris461767@gmail.com>
2020-08-25 19:03:04 +0100

    Added videolist component

---
+++ web/src/components/Videolist/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './index.scss'
+
+const Videolist = () => {
+    return (
+        <div className="videolist">
+            <div className="videolist__video"></div>
+            <div className="videolist__video"></div>
+            <div className="videolist__video"></div>
+            <div className="videolist__video"></div>
+            <div className="videolist__video"></div>
+            <div className="videolist__video"></div>
+            <div className="videolist__video"></div>
+            <div className="videolist__video"></div>
+            <div className="videolist__video"></div>
+        </div>
+    )
+}
+
+export default Videolist
