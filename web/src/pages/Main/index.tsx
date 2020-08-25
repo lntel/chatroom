c382efde217c7aae3409100f4b660e37b32ef588
Casper <joeharris461767@gmail.com>
2020-08-25 19:52:34 +0100

    Added socket connection Added debugging stream

--- web/src/pages/Main/index.tsx
+++ web/src/pages/Main/index.tsx
@@ -1,24 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import io from 'socket.io-client'
 import Controls from '../../components/Controls'
 import Userlist from '../../components/Userlist'
 import Videolist from '../../components/Videolist'
+import { UserStream } from '../../types'
 import './index.scss'
 
 const Main = () => {
+    const [streams, setStreams] = useState<UserStream[]>([]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:4000');
+
+        socket.on('disconnect', () => {
+            console.log('dis');
+        })
+    }, []);
 
     const handleMicEvent = (e: boolean) => {
         console.log(e)
     }
 
-    const handleStreamEvent = () => {
+    const handleStreamEvent = (e: MediaStream) => {
         console.log("streaming")
+
+        setStreams(old => [
+            ...old,
+            {
+                stream: e,
+                user: {
+                    nickname: 'test',
+                    peerId: 'tedfcsmf',
+                    self: true
+                }
+            }
+        ]);
+    }
+
+    const handleStreamEnded = (id: string) => {
+        setStreams([
+            ...streams.filter(stream => stream.user.peerId !== id)
+        ]);
     }
 
     return (
         <div className="main-page">
             <Userlist />
-            <Videolist />
-            <Controls onMicEvent={(e: boolean) => handleMicEvent(e)} onStreamEvent={() => handleStreamEvent()} />
+            <Videolist streams={streams} onStreamEnded={(id: string) => handleStreamEnded(id)} />
+            <Controls onMicEvent={(e: boolean) => handleMicEvent(e)} onStreamEvent={(e) => handleStreamEvent(e)} />
         </div>
     )
 }
