fe5f7947d0d8ee40cf46546577715d38c123614f
Casper <joeharris461767@gmail.com>
2020-08-25 19:03:30 +0100

    Added controls component

---
+++ web/src/components/Controls/index.tsx
@@ -0,0 +1,64 @@
+import React, { FC, useState } from 'react'
+import './index.scss'
+
+import ScreenShareIcon from '@material-ui/icons/ScreenShare';
+import CameraAltIcon from '@material-ui/icons/CameraAlt';
+import MicOffIcon from '@material-ui/icons/MicOff';
+import MicIcon from '@material-ui/icons/Mic';
+
+interface ControlsProps {
+    onMicEvent: (e: boolean) => void
+    onStreamEvent: () => void
+}
+
+const Controls: FC<ControlsProps> = ({ onMicEvent, onStreamEvent }) => {
+    const [muted, setMuted] = useState<boolean>(true);
+
+    const handleMicChange = () => {
+
+        onMicEvent(!muted);
+
+        setMuted(!muted);
+    }
+
+    const handleScreenshare = async () => {
+
+        const mediaDevices = navigator.mediaDevices as any;
+
+        const stream = await mediaDevices.getDisplayMedia({
+            video: true,
+            audio: false
+        });
+    }
+
+    const handleWebcam = async () => {
+
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+        });
+
+        console.log(stream)
+
+    }
+
+    return (
+        <div className="controls">
+            <button className="controls__camera" onClick={() => handleWebcam()}>
+                <CameraAltIcon>Filled</CameraAltIcon>
+            </button>
+            <button className="controls__screenshare" onClick={() => handleScreenshare()}>
+                <ScreenShareIcon>Filled</ScreenShareIcon>
+            </button>
+            <button className={muted ? "controls__microphone controls__microphone--muted" : "controls__microphone controls__microphone--unmuted"} onClick={() => handleMicChange()}>
+                { muted ? (
+                    <MicOffIcon>Filled</MicOffIcon>
+                ) : (
+                    <MicIcon>Filled</MicIcon>
+                ) }
+            </button>
+        </div>
+    )
+}
+
+export default Controls
