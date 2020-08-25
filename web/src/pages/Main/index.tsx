0ff219c8a560b8587617db097e5b4b5441154e9c
Casper <joeharris461767@gmail.com>
2020-08-25 19:02:51 +0100

    Added main component

--- web/src/pages/Main/index.tsx
+++ web/src/pages/Main/index.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Controls from '../../components/Controls'
+import Userlist from '../../components/Userlist'
+import Videolist from '../../components/Videolist'
+import './index.scss'
 
 const Main = () => {
+
+    const handleMicEvent = (e: boolean) => {
+        console.log(e)
+    }
+
+    const handleStreamEvent = () => {
+        console.log("streaming")
+    }
+
     return (
-        <div>
-            test
+        <div className="main-page">
+            <Userlist />
+            <Videolist />
+            <Controls onMicEvent={(e: boolean) => handleMicEvent(e)} onStreamEvent={() => handleStreamEvent()} />
         </div>
     )
 }
