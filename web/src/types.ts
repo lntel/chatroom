6113d6cbb4d4e66003c2567e78c62b1ffb487815
Casper <joeharris461767@gmail.com>
2020-08-26 11:57:37 +0100

    Added ChatMessage interface

--- web/src/types.ts
+++ web/src/types.ts
@@ -9,6 +9,13 @@ export interface UserStream {
     user: User
 }
 
+export interface ChatMessage {
+    id: string
+    content: string
+    user: User
+    postedDate: number | string | Date
+}
+
 export type Colors = | '#383C4A' | '#404552' | '#4B5162' | '#7C818C' | '#f3f3f3' | '#5294E2';
 
 export enum ClientEvents {
