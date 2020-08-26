61e1d4a977643eb01cbb498e3cf37aab34aa2d7f
BigMun <58153865+BigMun@users.noreply.github.com>
2020-08-26 10:36:51 +0100

    Added comments

--- server/src/entity/user.ts
+++ server/src/entity/user.ts
@@ -1,18 +1,22 @@
 import { Entity, Column, BaseEntity, PrimaryGeneratedColumn} from 'typeorm'
 import {Field, ObjectType} from 'type-graphql'
 
+//Users table entity
 @Entity()
 @ObjectType()
 export default class User extends BaseEntity {
 
+    //Id field
     @Field()
     @PrimaryGeneratedColumn()
     id: number;
 
+    //Username field
     @Field()
     @Column(() => String)
     username: string;
 
+    //Password field
     @Field()
     @Column(() => String)
     password: string;
