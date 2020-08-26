606b17805b4a3b2b9037e7a042e8c2884acd8b1b
Casper <joeharris461767@gmail.com>
2020-08-26 11:04:57 +0100

    Added colors Added client events

--- web/src/types.ts
+++ web/src/types.ts
@@ -8,3 +8,14 @@ export interface UserStream {
     stream: MediaStream
     user: User
 }
+
+export type Colors = | '#383C4A' | '#404552' | '#4B5162' | '#7C818C' | '#f3f3f3' | '#5294E2';
+
+export enum ClientEvents {
+    setNickname = 'set:nickname',
+    sendMessage = 'send:message',
+    userJoined = 'user:joined',
+    userLeft = 'user:left',
+    userList = 'user:list',
+    disconnect = 'disconnect'
+}
