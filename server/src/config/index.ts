98ac29d66e95643de4a2448aa9feb6d7e79b97e4
BigMun <58153865+BigMun@users.noreply.github.com>
2020-08-26 14:09:26 +0100

    Created MongoDB connection

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,6 +2,9 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-export default {
-    apiPort: process.env.API_PORT || 4000
+const config = {
+    apiPort: process.env.API_PORT || 4000,
+    mongoUrl: process.env.MONGO_URL || 'mongodb+srv://api:9pxDrZaADnafS2c3@chatroom.j7iv3.mongodb.net/chatroom?retryWrites=true&w=majority'
 }
+
+export default config;
