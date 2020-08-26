cb90171a8a645b49065f1fb15eeda3dc8e39130b
Casper <joeharris461767@gmail.com>
2020-08-26 11:58:10 +0100

    Added messages Added textbox processing

--- web/src/components/Chat/index.tsx
+++ web/src/components/Chat/index.tsx
@@ -1,20 +1,46 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+import { ChatMessage } from '../../types'
 import Textbox from '../Textbox'
 import { SlideInRight } from '../Transitions'
 import './index.scss'
 
 interface ChatProps {
     visible: boolean
+    messages: ChatMessage[]
+    onMessage: (content: string) => void
 }
 
-const Chat: FC<ChatProps> = ({ visible }) => {
+const Chat: FC<ChatProps> = ({ visible, messages, onMessage }) => {
+    const [messageInput, setMessageInput] = useState<string>('');
+
+    const handleMessageSend = () => {
+        onMessage(messageInput);
+    }
+    
     return (
         <SlideInRight state={visible}>
             <div className="chat">
                 <div className="chat__container">
-
+                    { messages && messages.length ? messages.map(message =>
+                        <div className="chat__message">
+                            <span className="chat__message__nickname">
+                                { message.user.nickname }
+                            </span>
+                            <p>
+                                { message.content }
+                            </p>
+                        </div>
+                    ) : null }
                 </div>
-                <Textbox bgColor="#4B5162" fgColor="#f3f3f3" className="chat__input" padding="1.7em" placeholder="Enter a message" />
+                <Textbox 
+                bgColor="#4B5162" 
+                fgColor="#f3f3f3" 
+                className="chat__input" 
+                padding="1.7em" 
+                placeholder="Enter a message" 
+                onChange={(v: string) => setMessageInput(v)} 
+                onEnter={() => handleMessageSend()}
+                value={messageInput} />
             </div>
         </SlideInRight>
     )
