b9b597906d5ff00df8cd97d3791340a8fdcafdb8
Casper <joeharris461767@gmail.com>
2020-08-26 14:18:51 +0100

    Removed credentials

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const config = {
     apiPort: process.env.API_PORT || 4000,
-    mongoUrl: process.env.MONGO_URL || 'mongodb+srv://api:9pxDrZaADnafS2c3@chatroom.j7iv3.mongodb.net/chatroom?retryWrites=true&w=majority'
+    mongoUrl: process.env.MONGO_URL || ''
 }
 
 export default config;
