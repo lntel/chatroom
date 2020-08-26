3311d1db15bfd4828589028cf04b6e48c6e07dcf
Casper <joeharris461767@gmail.com>
2020-08-26 12:44:03 +0100

    Made into an overlay

--- web/src/components/Userlist/index.tsx
+++ web/src/components/Userlist/index.tsx
@@ -1,12 +1,14 @@
 import React, { FC, useState } from 'react'
 import { User } from '../../types';
+import { SlideInLeft, SlideInRight } from '../Transitions';
 import './index.scss'
 
 interface UserlistProps {
     users?: User[]
+    visible: boolean
 }
 
-const Userlist: FC<UserlistProps> = ({ users }) => {
+const Userlist: FC<UserlistProps> = ({ users, visible }) => {
     // const [users, setUsers] = useState<User[]>([
     //     {
     //         nickname: 'Joe',
@@ -19,18 +21,20 @@ const Userlist: FC<UserlistProps> = ({ users }) => {
     // ]);
 
     return (
-        <div className="userlist">
-            <h1 className="userlist__title">
-                Participants
-            </h1>
-            <div className="userlist__users">
-                { users && users.length ? users.map(user =>
-                    <div className="userlist__user" key={user.peerId}>
-                        { user.nickname }
-                    </div>
-                ) : null }
+        <SlideInLeft state={visible}>
+            <div className="userlist">
+                <h1 className="userlist__title">
+                    Participants
+                </h1>
+                <div className="userlist__users">
+                    { users && users.length ? users.map(user =>
+                        <div className="userlist__user" key={user.peerId}>
+                            { user.nickname }
+                        </div>
+                    ) : null }
+                </div>
             </div>
-        </div>
+        </SlideInLeft>
     )
 }
 
