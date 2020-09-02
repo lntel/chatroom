fc745933c17f30e7fcebfedfa781e28e1fab6204
Casper <joeharris461767@gmail.com>
2020-09-02 14:39:49 +0100

    Implemented speaking sound meter

--- web/src/components/Video/index.tsx
+++ web/src/components/Video/index.tsx
@@ -1,5 +1,8 @@
-import React, { FC, useEffect, useRef } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
+
 import { User } from '../../types';
+
+import SoundMeter from '../../soundMeter'
 
 import CloseIcon from '@material-ui/icons/Close';
 
@@ -10,20 +13,38 @@ interface VideoProps {
 }
 
 const Video: FC<VideoProps> = ({ stream, onStreamEnded, user }) => {
+    const [speaking, setSpeaking] = useState<string>('0');
+
     const videoRef = useRef<HTMLVideoElement>(null);
 
     useEffect(() => {
         videoRef.current!.srcObject = stream;
+        
+        const audioContext = new AudioContext();
 
-        stream.getVideoTracks()[0].onended = function(e) {
-            onStreamEnded(user.peerId)
-        }
+        const soundMeter = new SoundMeter(audioContext);
 
+        soundMeter.connectToSource(stream, (e) => {
+            if(e) return console.error(e);
+
+            const interval = setInterval(() => {
+                setSpeaking(soundMeter.instant.toFixed(2));
+            }, 100);
+
+            stream.getVideoTracks()[0].onended = function(e) {
+
+                soundMeter.stop();
+
+                clearInterval(interval);
+
+                onStreamEnded(user.peerId)
+            }
+        })
         
     }, []);
 
     return (
-        <div className="videolist__video">
+        <div className={speaking !== '0.00' ? "videolist__video videolist__video--speaking" : "videolist__video"}>
             <div className="videolist__video__data">
                 <CloseIcon>Filled</CloseIcon>
             </div>
