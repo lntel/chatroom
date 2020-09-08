fe8c516c990103c037bb084afff2952e7bc4dc3c
lntel <joeharris461767@gmail.com>
2020-09-08 17:14:46 +0100

    Implemented socket context provider

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Route, Switch } from 'react-router';
 import SettingsContextProvider from './context/SettingsContext';
+import SocketContextProvider from './context/SocketContext';
 import Main from './pages/Main';
 
 const App = () => {
     return (
-        <SettingsContextProvider>
-            <Switch>
-                <Route to="/" component={Main} />
-            </Switch>
-        </SettingsContextProvider>
+        <SocketContextProvider>
+            <SettingsContextProvider>
+                <Switch>
+                    <Route to="/" component={Main} />
+                </Switch>
+            </SettingsContextProvider>
+        </SocketContextProvider>
     );
 }
 
